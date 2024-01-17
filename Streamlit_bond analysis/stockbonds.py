2350cf75cfc6d455d2e639fb7b7f5cb1d62aef1f
ADWOA M <adom0021@gmail.com>
2024-01-16 19:09:06 -0500

    Added Financial App Streamlit

---
+++ Streamlit_bond analysis/stockbonds.py
@@ -0,0 +1,118 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import plotly.graph_objects as go
+
+#Read and access individual stocks in S&P500
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+sp_table = pd.read_html(url)
+sp_tickers = sp_table[0]['Symbol'].to_list()
+
+tickers = st.multiselect("Select your stocks",sp_tickers)
+
+if not tickers:
+    st.error("Please select at least one ticker.")
+else:
+    periods = ['1mo', '6mo', '1y','2y','3y']
+    period = st.sidebar.selectbox('Period', periods, index=2)
+    figs = {}
+
+
+
+    total_so_far = yf.Ticker(tickers[0]).history(interval='1d', period=period)['Close']
+    
+    for i in range(len(tickers)):
+        hist = yf.Ticker(tickers[i]).history(interval='1d', period=period)
+        total_so_far = total_so_far.add(hist['Close'])
+        fig = go.Scatter(x=hist.index, y=hist['Close'], mode='lines', name=tickers[i])
+        figs[tickers[i]] = fig
+
+    tickerSymbol = st.sidebar.selectbox('Stock ticker', tickers)
+    show_all = st.sidebar.checkbox('Show all?')
+    fig = go.Figure()
+    if show_all:
+        total_fig = go.Scatter(
+            x=total_so_far.index,
+            y=total_so_far, mode='lines',
+            name='Total Value'
+
+        )
+        fig.add_trace(total_fig)
+
+        for ticker in figs:
+            fig.add_trace(figs[ticker])
+    else:
+        fig.add_trace(figs[tickerSymbol])
+
+    fig.update_layout(
+        title="Closing Stock Price",
+        xaxis_title="Date",
+        yaxis_title="Price ($)",
+        legend_title="Stocks",
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    data = yf.Ticker(tickerSymbol).info
+
+    #CSV Data for top 5 stocks
+    top5 = yf.download(tickers,interval='1d',period=period)['Close']
+
+    # Readimg S&P 500 from closing prices
+    sp500_cs = Path("SP500_historical.csv")
+    sp500_df = pd.read_csv(sp500_cs, index_col="Date", parse_dates=True)
+    sp500_df.sort_index(inplace=True)
+    # Rename `Close` Column to be specific to this portfolio.
+    sp500_df.columns=["S&P500"]
+
+    df_combined = pd.concat([top5, sp500_df], axis='columns')
+
+    #Sharpe Ratio for relationship between bonds and S&P500
+    # read ibond csv file
+    ibonds_path= Path("ibonds_cleaned_data.csv")
+    ibonds_df = pd.read_csv(ibonds_path)
+
+    #Set date as index and drop column, "Unnamed:0"
+    ibonds_df.set_index("date").head(10)
+    ibonds_df.drop(columns=['Unnamed: 0'], inplace=True)
+
+    #sum fixed rate and 6-month inflation rate 
+    ibonds_df['total_inflation rate']= ibonds_df['fixed rate'] + ibonds_df['six-month inflation rate']
+
+    # drop fixed rate and six-month inflation rate to ease standard deviation calculation
+    new_ibondsdf= ibonds_df[['total_inflation rate']]
+    #calculate standard deviation
+    st_dev = new_ibondsdf.std()
+    #Get yearly standard deviation
+    yearly_std = st_dev * np.sqrt(252)
+    # Get the sharpe ratio
+    sharpe= (new_ibondsdf.mean()*252)/ (yearly_std)
+    # First, get the daily standard deviations of all portfolios
+    #Visualize sharpe ratio for Ibond
+    #plot_1 = sharpe.plot(kind='bar')
+    st.title("Sharpe Ratio For IBonds")
+    st.bar_chart(sharpe)
+
+    std_dev = df_combined.std()
+    # Calculate the annualized standard deviation using 252 trading days
+    annual_std = std_dev * np.sqrt(252)
+    sharpe_ratio = (df_combined.mean()*252)/ (annual_std)
+    #print(sharpe_ratio.sort_values(ascending=False))
+
+    st.title("Sharpe Ratio For Stocks and S&P500 ")
+    st.bar_chart(sharpe_ratio)
+
+
+
+
+
+
+    
+
+
+
+
+
+    
